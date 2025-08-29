b092d25a7f41767c8bcb8debcf59df2ebf56c4d1
unknown <ibrahimabiodun2019@gmail.com>
2025-08-30 00:39:43 +0100

    prettier

--- index.js
+++ index.js
@@ -3,73 +3,68 @@ const submit = document.getElementById("submit");
 const message = document.getElementById("message");
 let result = randomNumber();
 let attempts = 0;
-message.style.marginBottom = "5px"
+message.style.marginBottom = "5px";
 
 // Add event listener
 submit.addEventListener("click", guess);
 
 // Random numbers function
-function randomNumber () {
-    return Math.floor(Math.random() * 10) + 1;
+function randomNumber() {
+  return Math.floor(Math.random() * 10) + 1;
 }
 
 // Reset and Add event listener function
-function endGame () {
-    submit.removeEventListener("click", guess);
-    submit.addEventListener("click", resetGame)
+function endGame() {
+  submit.removeEventListener("click", guess);
+  submit.addEventListener("click", resetGame);
 }
 
 // Reset function
-function resetGame () {
-    result = randomNumber();
-    attempts = 0;
-    guessingInput.value = "";
-    message.innerText = "Game reset! Enter a new guess between 1 and 10.";
-    message.style.color = "black";
-    submit.innerHTML = "Start over!";
+function resetGame() {
+  result = randomNumber();
+  attempts = 0;
+  guessingInput.value = "";
+  message.innerText = "Game reset! Enter a new guess between 1 and 10.";
+  message.style.color = "black";
+  submit.innerHTML = "Start over!";
 
-    submit.removeEventListener("click", resetGame);
-    submit.addEventListener("click", guess);
+  submit.removeEventListener("click", resetGame);
+  submit.addEventListener("click", guess);
 }
 
-
-
 // Main function
-function guess () {
-    let userGuess = Number(guessingInput.value)
-    const maxAttempts = 5
-    attempts++;
-    // empty input
-    if (!userGuess) {
-        message.innerText = "⚠️ Please enter a number between 1 and 10";
-        message.style.color = "#b60707ff";
-        return;
-    }
-
-    // validation
-    if (attempts >= maxAttempts && userGuess != result) {
-        message.innerText = `❌ Game Over! The number was ${result}.`;
-        message.style.color = "#b60707ff";
-        submit.innerHTML = "Start Over!";
-        endGame();
+function guess() {
+  let userGuess = Number(guessingInput.value);
+  const maxAttempts = 5;
+  attempts++;
+  // empty input
+  if (!userGuess) {
+    message.innerText = "⚠️ Please enter a number between 1 and 10";
+    message.style.color = "#b60707ff";
     return;
-    }
+  }
 
+  // validation
+  if (attempts >= maxAttempts && userGuess != result) {
+    message.innerText = `❌ Game Over! The number was ${result}.`;
+    message.style.color = "#b60707ff";
+    submit.innerHTML = "Start Over!";
+    endGame();
+    return;
+  }
 
-    if (userGuess > result) {
-         message.innerText = `🙄 Too high! Attempts left: ${maxAttempts - attempts}`;
-         message.style.color = "#b60707ff";
-         submit.innerHTML = "Try Again!";
-
-    } else if (userGuess < result) {
-        message.innerText = `🙄 Too low! Attempts left: ${maxAttempts - attempts}`;
-        message.style.color = "#b60707ff";
-        submit.innerHTML = "Try Again!";
-    
-    } else {
-        message.innerText = `🎉 Awesome! You nailed it — the secret number was ${result}. You got it in just ${attempts} attempts!`
-        message.style.color = "#130f0fff"
-        submit.innerHTML = "Start Over!"
-        endGame()
-    }
+  if (userGuess > result) {
+    message.innerText = `🙄 Too high! Attempts left: ${maxAttempts - attempts}`;
+    message.style.color = "#b60707ff";
+    submit.innerHTML = "Try Again!";
+  } else if (userGuess < result) {
+    message.innerText = `🙄 Too low! Attempts left: ${maxAttempts - attempts}`;
+    message.style.color = "#b60707ff";
+    submit.innerHTML = "Try Again!";
+  } else {
+    message.innerText = `🎉 Awesome! You nailed it — the secret number was ${result}. You got it in just ${attempts} attempts!`;
+    message.style.color = "#130f0fff";
+    submit.innerHTML = "Start Over!";
+    endGame();
+  }
 }
